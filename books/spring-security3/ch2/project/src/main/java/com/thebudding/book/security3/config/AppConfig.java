f132d63dc59bfdcaffccfae90ee0e3e0c6f54f2f
Dec7 <denux7@gmail.com>
2018-11-18 19:02:18 +0900

    [SpringSecurity3-Ch2] UnanimousBased 설정

--- books/spring-security3/ch2/project/src/main/java/com/thebudding/book/security3/config/AppConfig.java
+++ books/spring-security3/ch2/project/src/main/java/com/thebudding/book/security3/config/AppConfig.java
@@ -1,10 +1,17 @@
 package com.thebudding.book.security3.config;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.security.access.AccessDecisionVoter;
+import org.springframework.security.access.vote.AuthenticatedVoter;
+import org.springframework.security.access.vote.RoleVoter;
+import org.springframework.security.access.vote.UnanimousBased;
 import org.springframework.stereotype.Controller;
 
 @Configuration
@@ -24,4 +31,11 @@ import org.springframework.stereotype.Controller;
 @ImportResource({"classpath:security-context.xml"})
 public class AppConfig {
 
+  @Bean
+  public UnanimousBased unanimousBased() {
+    List<AccessDecisionVoter> list = new ArrayList<>();
+    list.add(new RoleVoter());
+    list.add(new AuthenticatedVoter());
+    return new UnanimousBased(list);
+  }
 }
