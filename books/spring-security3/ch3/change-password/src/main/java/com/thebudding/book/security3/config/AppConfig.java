f95efc13e9c84194a8b3e67ed4016421efedffff
Dec7 <denux7@gmail.com>
2018-12-01 19:00:49 +0900

    [SpringSecurity3] Add change password example

---
+++ books/spring-security3/ch3/change-password/src/main/java/com/thebudding/book/security3/config/AppConfig.java
@@ -0,0 +1,66 @@
+package com.thebudding.book.security3.config;
+
+import com.thebudding.book.security3.service.IPTokenBasedRememberMeServices;
+import com.thebudding.book.security3.service.InMemoryChangePasswordDaoImpl;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.security.access.AccessDecisionVoter;
+import org.springframework.security.access.vote.AuthenticatedVoter;
+import org.springframework.security.access.vote.RoleVoter;
+import org.springframework.security.access.vote.UnanimousBased;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Controller;
+
+@Configuration
+@ComponentScan(
+    basePackages = {
+        "com.thebudding.book.security3"
+    },
+    excludeFilters={
+        @Filter(
+            type = FilterType.ANNOTATION,
+            value = {
+                Configuration.class,
+                Controller.class
+            })
+    }
+)
+@ImportResource({"classpath:security-context.xml"})
+public class AppConfig {
+
+  @Autowired
+  private UserDetailsService userDetailsService;
+
+  @Bean
+  public IPTokenBasedRememberMeServices ipTokenBasedRememberMeServicesBean() {
+    return new IPTokenBasedRememberMeServices("jbcpPetStore", userDetailsService,
+        "_remember_me", "REMEMBER_ME");
+  }
+
+  @Bean
+  public UnanimousBased unanimousBased() {
+    List<AccessDecisionVoter> list = new ArrayList<>();
+    list.add(new RoleVoter());
+    list.add(new AuthenticatedVoter());
+    return new UnanimousBased(list);
+  }
+
+  @Bean
+  public InMemoryChangePasswordDaoImpl userService() {
+    Properties properties = new Properties();
+    properties.setProperty("guest", "guest,ROLE_USER");
+
+    InMemoryChangePasswordDaoImpl userService = new InMemoryChangePasswordDaoImpl();
+    userService.setUserProperties(properties);
+
+    return userService;
+  }
+}
