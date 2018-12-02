67ac7bf85b7c3a33a942a3c3383d40da2ae5c61d
Dec7 <denux7@gmail.com>
2018-12-02 13:45:06 +0900

    [SpringSecurity3] Apply salt for encryption of password

--- books/spring-security3/ch4/project/src/main/java/com/thebudding/book/security3/service/DatabasePasswordSecurerBean.java
+++ books/spring-security3/ch4/project/src/main/java/com/thebudding/book/security3/service/DatabasePasswordSecurerBean.java
@@ -4,12 +4,21 @@ import java.sql.ResultSet;
 import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import org.springframework.security.authentication.dao.ReflectionSaltSource;
 import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 public class DatabasePasswordSecurerBean extends JdbcDaoSupport {
 
   @Autowired
   private ShaPasswordEncoder passwordEncoder;
+
+  @Autowired
+  private ReflectionSaltSource saltSource;
+
+  @Autowired
+  private UserDetailsService userDetailsService;
 
   @PostConstruct
   private void init() {
@@ -20,7 +29,9 @@ public class DatabasePasswordSecurerBean extends JdbcDaoSupport {
     getJdbcTemplate().query("select username, password from users", (ResultSet rs) -> {
       String username = rs.getString(1);
       String password = rs.getString(2);
-      String encodedPassword = passwordEncoder.encodePassword(password, null);
+
+      UserDetails user = userDetailsService.loadUserByUsername(username);
+      String encodedPassword = passwordEncoder.encodePassword(password, saltSource.getSalt(user));
       getJdbcTemplate().update(
           "update users set password = ? where username = ?", encodedPassword, username);
       logger.debug("Updating password for username: " + username + " to: " + encodedPassword);
