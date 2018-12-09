0dfbda659ffc42bede69fa0ab2c2f8a0620e53c2
Dec7 <denux7@gmail.com>
2018-12-09 12:35:22 +0900

    [SpringSecurity3] Add user number logged in

--- books/spring-security3/ch6/project/WebContent/WEB-INF/views/common/footer.jsp
+++ books/spring-security3/ch6/project/WebContent/WEB-INF/views/common/footer.jsp
@@ -1,3 +1,6 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<div id="footer">
+    ${numUsers} user(s) are logged in!
+</div>
 </body>
 </html>
--- books/spring-security3/ch6/project/src/main/java/com/thebudding/book/security3/controller/BaseController.java
+++ books/spring-security3/ch6/project/src/main/java/com/thebudding/book/security3/controller/BaseController.java
@@ -1,11 +1,21 @@
 package com.thebudding.book.security3.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 public class BaseController {
+
+  @Autowired
+  private SessionRegistry sessionRegistry;
+
+  @ModelAttribute("numUsers")
+  public int getNumberOfUsers() {
+    return sessionRegistry.getAllPrincipals().size();
+  }
 
   protected Authentication getAuthentication() {
     return SecurityContextHolder.getContext().getAuthentication();
