6e23bd733bf0c1c9679a360d272eb7e5c3426d9e
Dec7 <denux7@gmail.com>
2019-03-03 17:52:52 +0900

    item30 generic singleton

--- books/effective-java-3e/project/src/main/java/com/thebudding/book/effectivejava/item30/GenericMethod.java
+++ books/effective-java-3e/project/src/main/java/com/thebudding/book/effectivejava/item30/GenericMethod.java
@@ -2,13 +2,21 @@ package com.thebudding.book.effectivejava.item30;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.function.UnaryOperator;
 
 public class GenericMethod {
+
+  private static UnaryOperator<Object> IDENTITY_FN = (t) -> t;
 
   public static <E> Set<E> union(Set<E> s1, Set<E> s2) {
     Set<E> result = new HashSet<>(s1);
     result.addAll(s2);
     return result;
+  }
+
+  @SuppressWarnings("unchecked")
+  public static <T> UnaryOperator<T> identityFunction() {
+    return (UnaryOperator<T>) IDENTITY_FN;
   }
 
 }
--- books/effective-java-3e/project/src/test/java/com/thebudding/book/effectivejava/item30/GenericMethodTest.java
+++ books/effective-java-3e/project/src/test/java/com/thebudding/book/effectivejava/item30/GenericMethodTest.java
@@ -3,6 +3,7 @@ package com.thebudding.book.effectivejava.item30;
 import static org.junit.Assert.*;
 
 import java.util.Set;
+import java.util.function.UnaryOperator;
 import org.junit.Test;
 
 public class GenericMethodTest {
@@ -13,5 +14,20 @@ public class GenericMethodTest {
     Set<String> b = Set.of("a","b","c");
     Set<String> all = GenericMethod.union(a, b);
     System.out.println(all);
+  }
+
+  @Test
+  public void test2() {
+    String[] str = { "a", "b", "c" };
+    UnaryOperator<String> strOp = GenericMethod.identityFunction();
+    for (String s : str) {
+      System.out.println(strOp.apply(s));
+    }
+
+    Number[] num = { 1, 2.0, 3L };
+    UnaryOperator<Number> numOp = GenericMethod.identityFunction();
+    for (Number n : num) {
+      System.out.println(numOp.apply(n));
+    }
   }
 }
