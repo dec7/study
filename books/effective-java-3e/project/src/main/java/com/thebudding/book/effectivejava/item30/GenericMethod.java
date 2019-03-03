aaa97d723096e00dd06cfd758458168fa40c9f5c
Dec7 <denux7@gmail.com>
2019-03-03 17:47:45 +0900

    item30 generic

--- books/effective-java-3e/project/src/main/java/com/thebudding/book/effectivejava/item30/GenericMethod.java
+++ books/effective-java-3e/project/src/main/java/com/thebudding/book/effectivejava/item30/GenericMethod.java
@@ -5,8 +5,8 @@ import java.util.Set;
 
 public class GenericMethod {
 
-  public static Set union(Set s1, Set s2) {
-    Set result = new HashSet(s1);
+  public static <E> Set<E> union(Set<E> s1, Set<E> s2) {
+    Set<E> result = new HashSet<>(s1);
     result.addAll(s2);
     return result;
   }
