569388ee6aa8b70234efda6d4c8c58f3e2f14f7d
Dec7 <denux7@gmail.com>
2019-03-03 18:32:24 +0900

    item31

---
+++ books/effective-java-3e/project/src/main/java/com/thebudding/book/effectivejava/item31/GenericMethod.java
@@ -0,0 +1,39 @@
+package com.thebudding.book.effectivejava.item31;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.function.UnaryOperator;
+
+public class GenericMethod {
+
+  private static UnaryOperator<Object> IDENTITY_FN = (t) -> t;
+
+  public static <E> Set<E> union(Set<E> s1, Set<E> s2) {
+    Set<E> result = new HashSet<>(s1);
+    result.addAll(s2);
+    return result;
+  }
+
+  @SuppressWarnings("unchecked")
+  public static <T> UnaryOperator<T> identityFunction() {
+    return (UnaryOperator<T>) IDENTITY_FN;
+  }
+
+
+  public static <E extends Comparable<E>> E max(Collection<E> c) {
+    if (c.isEmpty()) {
+      throw new IllegalArgumentException("Collection is empty.");
+    }
+
+    E result = null;
+    for (E e : c) {
+      if (result == null || e.compareTo(result) > 0) {
+        result = Objects.requireNonNull(e);
+      }
+    }
+    return result;
+  }
+
+}
