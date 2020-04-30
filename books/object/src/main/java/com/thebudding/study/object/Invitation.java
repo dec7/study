4e0e6c2ce1a5e02fbcc7fb2e23346cd83872dbae
Dec7 <denux@gmail.com>
2020-04-30 10:47:18 +0900

    [Ch1-1] Add example codes

---
+++ books/object/src/main/java/com/thebudding/study/object/Invitation.java
@@ -0,0 +1,7 @@
+package com.thebudding.study.object;
+
+import java.time.LocalDateTime;
+
+public class Invitation {
+  private LocalDateTime when;
+}
---
+++ books/object/src/main/java/com/thebudding/study/object/Ticket.java
@@ -0,0 +1,9 @@
+package com.thebudding.study.object;
+
+public class Ticket {
+  private Long fee;
+
+  public Long getFee() {
+    return fee;
+  }
+}
