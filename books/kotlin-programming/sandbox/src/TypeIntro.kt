d47702d9ffc203d9097ae98072d36363f509bc37
dec7 <denux7@gmail.com>
2021-05-30 19:58:15 +0900

    [KotlinProgramming][Ch2] 타입 활용

--- books/kotlin-programming/sandbox/src/TypeIntro.kt
+++ books/kotlin-programming/sandbox/src/TypeIntro.kt
@@ -1,5 +1,16 @@
+const val MAX_EXPERIENCE: Int = 5000
+
 fun main(args: Array<String>) {
-    var experiencePoints: Int = 5
+    val playerName = "에스트라곤"
+    var experiencePoints = 50
     experiencePoints += 5
+    var hasSteed = false
+    var drink = listOf<String>("벌꿀주","포도주","라크루아")
     println(experiencePoints)
+    println(playerName)
+
+    if (!hasSteed) {
+        println("The player does not have steed yet.")
+    }
+    println(playerName.reversed())
 }
