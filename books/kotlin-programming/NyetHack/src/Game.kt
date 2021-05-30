fafa3a7cea048d98bae4fe33fe73888a29f4e46c
dec7 <denux7@gmail.com>
2021-05-30 20:06:16 +0900

    [KotlinProgramming][Ch3] NyetHack 프로젝트 추가

---
+++ books/kotlin-programming/NyetHack/src/Game.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    var name = "마드리갈"
+    var healthPoints = 100
+
+    if (healthPoints == 100) {
+        println(name + " 최상의 상태임!")
+    } else {
+        println(name + " 최악의 상태임!")
+    }
+}
