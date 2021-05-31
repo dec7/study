ab9b7023b4490c834c1fab4e826e736748b1346c
dec7 <denux7@gmail.com>
2021-06-01 08:40:05 +0900

    [KotlinProgramming][Ch6] 예외 던지기

--- books/kotlin-programming/NyetHack/src/SwordJuggler.kt
+++ books/kotlin-programming/NyetHack/src/SwordJuggler.kt
@@ -1,3 +1,5 @@
+import java.lang.IllegalStateException
+
 fun main(args: Array<String>) {
     var swordsJuggling: Int? = null
     val isJugglingProficient = (1..3).shuffled().last() == 3
@@ -5,7 +7,12 @@ fun main(args: Array<String>) {
         swordsJuggling = 2
     }
 
+    proficiencyCheck(swordsJuggling)
     swordsJuggling = swordsJuggling!!.plus(1)
 
     println("$swordsJuggling 개의 칼로 저글링 합니다.")
+}
+
+fun proficiencyCheck(swordsJuggling: Int?) {
+    swordsJuggling ?: throw IllegalStateException("플레이어가 저글링을 할 수 없음")
 }
