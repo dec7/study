50ea5d63bff7c3c517cee837e146d771d7c8798f
dec7 <denux7@gmail.com>
2021-05-31 10:17:09 +0900

    [KotlinProgramming][Ch4] Extract healthStatus logic to method

--- books/kotlin-programming/NyetHack/src/GameCh3Challenge.kt
+++ books/kotlin-programming/NyetHack/src/GameCh3Challenge.kt
@@ -14,17 +14,7 @@ fun main(args: Array<String>) {
                 "(Blessed: ${if (isBlessed) "YES" else "NO"})"
     )
 
-    val healthStatus = when (healthPoints) {
-        100 -> "최상의 상태임!"
-        in 90..99 -> "약간의 찰과상만 있음"
-        in 75..89 -> if (isBlessed) {
-            "경미한 상처가 있지만 빨리 치유됨."
-        } else {
-            " 경미한 상처만 있음"
-        }
-        in 15..74 -> "많이 다친 것 같음"
-        else -> "최악의 상태임!"
-    }
+    val healthStatus = formatHealthStatus(healthPoints, isBlessed)
 
     println("$name $healthStatus")
 
@@ -46,4 +36,19 @@ fun main(args: Array<String>) {
         else -> ""
     }
     println("$karma -> $karmaColor")
+}
+
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String {
+    val healthStatus = when (healthPoints) {
+        100 -> "최상의 상태임!"
+        in 90..99 -> "약간의 찰과상만 있음"
+        in 75..89 -> if (isBlessed) {
+            "경미한 상처가 있지만 빨리 치유됨."
+        } else {
+            " 경미한 상처만 있음"
+        }
+        in 15..74 -> "많이 다친 것 같음"
+        else -> "최악의 상태임!"
+    }
+    return healthStatus
 }
