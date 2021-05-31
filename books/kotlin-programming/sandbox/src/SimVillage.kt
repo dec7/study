f184d006a8d7489942fd9a40123c5bfb50f29e66
dec7 <denux7@gmail.com>
2021-05-31 18:00:22 +0900

    [KotlinProgramming][Ch5] 단축문법 적용

--- books/kotlin-programming/sandbox/src/SimVillage.kt
+++ books/kotlin-programming/sandbox/src/SimVillage.kt
@@ -1,10 +1,9 @@
 fun main(args: Array<String>) {
-    val greetingFunction = { playerName: String, numBuildings: Int ->
+    runSimulation("김선달") { playerName: String, numBuildings: Int ->
         val currentYear = 2019
         println("$numBuildings 채의 건물이 추가됨.")
         "SimVillage 방문을 환영합니다, $playerName 님! (copyright $currentYear)"
     }
-    runSimulation("김선달", greetingFunction)
 }
 
 fun runSimulation(playerName: String, greetingFunction: (String, Int) -> String) {
