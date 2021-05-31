0a0ca8208a2b3a30949c0659d8fc88169973dd92
dec7 <denux7@gmail.com>
2021-05-31 17:56:51 +0900

    [KotlinProgramming][Ch5] 함수를 인자로 받는 함수 정의

--- books/kotlin-programming/sandbox/src/SimVillage.kt
+++ books/kotlin-programming/sandbox/src/SimVillage.kt
@@ -4,5 +4,10 @@ fun main(args: Array<String>) {
         println("$numBuildings 채의 건물이 추가됨.")
         "SimVillage 방문을 환영합니다, $playerName 님! (copyright $currentYear)"
     }
-    println(greetingFunction("김선달", 2))
+    runSimulation("김선달", greetingFunction)
+}
+
+fun runSimulation(playerName: String, greetingFunction: (String, Int) -> String) {
+    val numBuildings = (1..3).shuffled().last()
+    println(greetingFunction(playerName, numBuildings))
 }
