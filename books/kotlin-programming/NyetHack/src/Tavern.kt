7e3b3ecec184323a23ef65be710ae06dc85413a4
dec7 <denux7@gmail.com>
2021-05-31 23:25:01 +0900

    [KotlinProgramming][Ch6] non null assertion 연산자 적용

--- books/kotlin-programming/NyetHack/src/Tavern.kt
+++ books/kotlin-programming/NyetHack/src/Tavern.kt
@@ -1,11 +1,5 @@
 fun main(args: Array<String>) {
-    var beverage = readLine()?.let {
-        if (it.isNotBlank()) {
-            it.capitalize()
-        } else {
-            "맥주"
-        }
-    }
+    var beverage = readLine()!!.capitalize()
 //    beverage = null
     println(beverage)
 }
