c44d0ef026cf31cc124b4d5bc8256e99497af26f
dec7 <denux7@gmail.com>
2021-05-31 17:51:20 +0900

    [KotlinProgramming][Ch5] 함수인자 타입추론 적용

--- books/kotlin-programming/sandbox/src/SimVillage.kt
+++ books/kotlin-programming/sandbox/src/SimVillage.kt
@@ -1,7 +1,7 @@
 fun main(args: Array<String>) {
-    val greetingFunction: (String, Int) -> String = { playerName, numBildings ->
+    val greetingFunction = { playerName: String, numBuildings: Int ->
         val currentYear = 2019
-        println("$numBildings 채의 건물이 추가됨.")
+        println("$numBuildings 채의 건물이 추가됨.")
         "SimVillage 방문을 환영합니다, $playerName 님! (copyright $currentYear)"
     }
     println(greetingFunction("김선달", 2))
