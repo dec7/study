3e504f70287829a57ec6092cadf56929249c6660
dec7 <denux7@gmail.com>
2021-05-31 18:27:18 +0900

    [KotlinProgramming][Ch5] 함수 참조로 전달하기

--- books/kotlin-programming/sandbox/src/SimVillage.kt
+++ books/kotlin-programming/sandbox/src/SimVillage.kt
@@ -1,12 +1,23 @@
 fun main(args: Array<String>) {
-    runSimulation("김선달") { playerName: String, numBuildings: Int ->
+    runSimulation("김선달", ::printConstructionCost)
+    { playerName: String, numBuildings: Int ->
         val currentYear = 2019
         println("$numBuildings 채의 건물이 추가됨.")
         "SimVillage 방문을 환영합니다, $playerName 님! (copyright $currentYear)"
     }
 }
 
-inline fun runSimulation(playerName: String, greetingFunction: (String, Int) -> String) {
+inline fun runSimulation(
+    playerName: String,
+    costPrinter: (Int) -> Unit,
+    greetingFunction: (String, Int) -> String
+) {
     val numBuildings = (1..3).shuffled().last()
+    costPrinter(numBuildings)
     println(greetingFunction(playerName, numBuildings))
+}
+
+fun printConstructionCost(numBuildings: Int) {
+    val cost = 500
+    println("건축 비용: ${cost * numBuildings}")
 }
