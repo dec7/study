eec8dac9905c35a515fed96f563a7ff05996ba9d
dec7 <denux7@gmail.com>
2021-05-31 23:22:10 +0900

    [KotlinProgramming][Ch6] 안전호출연산자와 let함수 사용

--- books/kotlin-programming/NyetHack/src/Tavern.kt
+++ books/kotlin-programming/NyetHack/src/Tavern.kt
@@ -1,5 +1,11 @@
 fun main(args: Array<String>) {
-    var beverage = readLine()?.capitalize()
+    var beverage = readLine()?.let {
+        if (it.isNotBlank()) {
+            it.capitalize()
+        } else {
+            "맥주"
+        }
+    }
 //    beverage = null
     println(beverage)
 }
