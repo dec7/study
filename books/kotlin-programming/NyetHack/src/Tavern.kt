f0fcf5822fd0e37e889c1d4304d526aafd567f05
dec7 <denux7@gmail.com>
2021-05-31 23:37:44 +0900

    [KotlinProgramming][Ch6] null 복합연산자 적용 1

--- books/kotlin-programming/NyetHack/src/Tavern.kt
+++ books/kotlin-programming/NyetHack/src/Tavern.kt
@@ -2,11 +2,9 @@ fun main(args: Array<String>) {
     var beverage = readLine()
 //    beverage = null
 
-    if (beverage != null) {
-        beverage = beverage.capitalize()
-    } else {
-        println("beverage is null")
-    }
+    beverage?.let {
+        beverage = it.capitalize()
+    } ?: println ("beverage is null")
 
 //    println(beverage)
     val beverageServed: String = beverage ?: "맥주"
