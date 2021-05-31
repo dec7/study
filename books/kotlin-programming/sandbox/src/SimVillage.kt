d295b40e4b89354e39bbd0568afbb9f047fdb38e
dec7 <denux7@gmail.com>
2021-05-31 17:22:15 +0900

    [KotlinProgramming][Ch5] 함수 타입과 암시적 반환 적용

--- books/kotlin-programming/sandbox/src/SimVillage.kt
+++ books/kotlin-programming/sandbox/src/SimVillage.kt
@@ -1,6 +1,7 @@
 fun main(args: Array<String>) {
-    println({
+    val greetingFunction: () -> String = {
         val currentYear = 2019
         "SimVillage 방문을 환영합니다, 촌장님! (copyright $currentYear)"
-    }())
+    }
+    println(greetingFunction())
 }
