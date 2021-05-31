23202dee23a0a44d1b6157025867f635eec07d8a
dec7 <denux7@gmail.com>
2021-05-31 23:29:29 +0900

    [KotlinProgramming][Ch6] null여부를 if로 검사

--- books/kotlin-programming/NyetHack/src/Tavern.kt
+++ books/kotlin-programming/NyetHack/src/Tavern.kt
@@ -1,5 +1,12 @@
 fun main(args: Array<String>) {
-    var beverage = readLine()!!.capitalize()
+    var beverage = readLine()
 //    beverage = null
+
+    if (beverage != null) {
+        beverage = beverage.capitalize()
+    } else {
+        println("beverage is null")
+    }
+
     println(beverage)
 }
