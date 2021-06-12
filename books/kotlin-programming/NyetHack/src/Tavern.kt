7528ef0fe366cbb3a9f9623b7a66d47bf8d6348a
dec7 <denux7@gmail.com>
2021-06-12 12:13:55 +0900

    [KotlinProgramming][Ch7] 유니코드 적용

--- books/kotlin-programming/NyetHack/src/Tavern.kt
+++ books/kotlin-programming/NyetHack/src/Tavern.kt
@@ -2,6 +2,10 @@ const val TAVERN_NAME = "Taernyl's Folly"
 fun main(args: Array<String>) {
     //placeOrder("shandy,Dragon's Breath,5.91")
     placeOrder("elixir,Shirley's Temple,4.12")
+
+    val capitalA: Char = 'A'
+    val unicodeCapitalA: Char = '\u0041'
+    println("A: ${capitalA}, unicode A: ${unicodeCapitalA}")
 }
 
 private fun toDragonSpeak(phrase: String) =
