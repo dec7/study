9462be3747310e38b91087748238bf02cc0da1dc
dec7 <denux7@gmail.com>
2021-06-12 12:08:30 +0900

    [KotlinProgramming][Ch7] 문자열 비교하기

--- books/kotlin-programming/NyetHack/src/Tavern.kt
+++ books/kotlin-programming/NyetHack/src/Tavern.kt
@@ -1,6 +1,7 @@
 const val TAVERN_NAME = "Taernyl's Folly"
 fun main(args: Array<String>) {
-    placeOrder("shandy,Dragon's Breath,5.91")
+    //placeOrder("shandy,Dragon's Breath,5.91")
+    placeOrder("elixir,Shirley's Temple,4.12")
 }
 
 private fun toDragonSpeak(phrase: String) =
@@ -18,12 +19,11 @@ private fun toDragonSpeak(phrase: String) =
 fun placeOrder(menuData: String) {
     val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
     val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
-    println("마드리갈은 $tavernMaster 에게 주문한다.")
-
     val (type, name, price) = menuData.split(",")
-    val message = "마드리갈은 금화 $price 로 $name ($type)를 구입한다."
-    println(message)
-
-    val phrase = "와, $name 진자 좋구나!!"
-    println("마드리갈이 감탄한다: ${toDragonSpeak(phrase)}")
+    val phrase = if (name == "Dragon's Breath") {
+        "마드리갈이 감탄한다: ${toDragonSpeak("와, $name 진짜 좋구나.")}"
+    } else {
+        "마드리갈이 말한다: 감사합니다 $name"
+    }
+    println(phrase)
 }
