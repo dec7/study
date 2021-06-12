e38525f47ad418fadb9c903ddac5947af935c77d
dec7 <denux7@gmail.com>
2021-06-12 11:57:59 +0900

    [KotlinProgramming][Ch7] 문자열 변경하기

--- books/kotlin-programming/NyetHack/src/Tavern.kt
+++ books/kotlin-programming/NyetHack/src/Tavern.kt
@@ -3,6 +3,18 @@ fun main(args: Array<String>) {
     placeOrder("shandy,Dragon's Breath,5.91")
 }
 
+private fun toDragonSpeak(phrase: String) =
+    phrase.replace(Regex("[aeiou]")) {
+        when (it.value) {
+            "a" -> "4"
+            "e" -> "3"
+            "i" -> "1"
+            "o" -> "-"
+            "u" -> "|_|"
+            else -> it.value
+        }
+    }
+
 fun placeOrder(menuData: String) {
     val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
     val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
@@ -11,4 +23,7 @@ fun placeOrder(menuData: String) {
     val (type, name, price) = menuData.split(",")
     val message = "마드리갈은 금화 $price 로 $name ($type)를 구입한다."
     println(message)
+
+    val phrase = "와, $name 진자 좋구나!!"
+    println("마드리갈이 감탄한다: ${toDragonSpeak(phrase)}")
 }
