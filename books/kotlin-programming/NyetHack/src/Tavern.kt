071f0c0318ae9c6f43ec224adb1206cb5f50afa3
dec7 <denux7@gmail.com>
2021-06-12 21:06:10 +0900

    [KotlinProgramming][Ch10] List 요소 검사

--- books/kotlin-programming/NyetHack/src/Tavern.kt
+++ books/kotlin-programming/NyetHack/src/Tavern.kt
@@ -10,6 +10,20 @@ fun main(args: Array<String>) {
     placeOrder("shandy,Dragon's Breath,5.91")
 
     printPatron()
+
+    if (patronList.contains("Eli")) {
+        println("술집 주인이 말한다: Eli는 안쪽 방에서 카드하고 있어요.")
+    } else {
+        println("술집 주인이 말한다: Eli는 여기 없어요.")
+    }
+
+    if (patronList.containsAll(listOf("Sophie", "Mordoc"))) {
+        println("술집 주인이 말한다: 네, 모두 있어요.")
+    } else {
+        println("술집 주인이 말한다: 아니오, 나간 사람도 있습니다.")
+    }
+
+    placeOrder("shandy,Dragon's Breath,5.91")
 }
 
 private fun printPatron() {
