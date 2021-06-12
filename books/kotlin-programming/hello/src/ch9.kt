9ede003f955594585848b0460a583190add115ce
dec7 <denux7@gmail.com>
2021-06-12 16:34:29 +0900

    [KotlinProgramming][Ch9] apply 함수

---
+++ books/kotlin-programming/hello/src/ch9.kt
@@ -0,0 +1,13 @@
+import java.io.File
+
+fun main(args: Array<String>) {
+//    val menuFile = File("menu-file.txt")
+//    menuFile.setReadable(true)
+//    menuFile.setWritable(true)
+//    menuFile.setExecutable(false)
+    val menuFile = File("menu-file.txt").apply {
+        setReadable(true)
+        setWritable(true)
+        setExecutable(false)
+    }
+}
