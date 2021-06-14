5fc9982c65fd02456b6266d96878351dc4750232
dec7 <denux7@gmail.com>
2021-06-14 16:12:54 +0900

    [KotlinProgramming][Ch13] 지명인자 사용

--- books/kotlin-programming/NyetHack/src/com/bignerdranch/nyethack/Game.kt
+++ books/kotlin-programming/NyetHack/src/com/bignerdranch/nyethack/Game.kt
@@ -3,6 +3,7 @@ package com.bignerdranch.nyethack
 fun main(args: Array<String>) {
     val name = "마드리갈"
 
+    val player1 = Player(_name = "Madrigal", healthPoints = 100, isBlessed = false, isImmortal = false)
     val player = Player("Madrigal")
     println(player.name + "TheBrave")
     player.castFireball()
