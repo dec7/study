5bc0bcff564ddcad2fc040609c17d51598712ff2
dec7 <denux7@gmail.com>
2021-06-14 14:45:27 +0900

    [KotlinProgramming][Ch12] Player 리팩토링

--- books/kotlin-programming/NyetHack/src/Game.kt
+++ books/kotlin-programming/NyetHack/src/Game.kt
@@ -1,66 +1,27 @@
 fun main(args: Array<String>) {
     val name = "마드리갈"
-    val healthPoints = 87
-    val isBlessed = true
-    val isImmortal = false
 
     val player = Player()
     //player.name = "estragon "
     println(player.name + "TheBrave")
     player.castFireball()
 
-    val auraColor = auraColor(isBlessed, healthPoints, isImmortal)
-    val healthStatus = formatHealthStatus(healthPoints, isBlessed)
-    printPlayerStatus(auraColor, isBlessed, player.name, healthStatus)
-    val status = "Status"
-    printPlayerStatus(auraColor = "NONE", isBlessed = true, name = "마드리갈", healthStatus = status)
-
-    val karma = (Math.pow(Math.random(), (110 - healthPoints) / 100.0) * 20).toInt()
-    val karmaColor = when (karma) {
-        in 0..5 -> "red"
-        in 6..10 -> "orance"
-        in 11..15 -> "purple"
-        in 16..20 -> "green"
-        else -> ""
-    }
-    println("$karma -> $karmaColor")
-    `**~prolly not a good idea!~**`()
+    printPlayerStatus(player)
 
     val numLetters = "Mississippi".count()
     println(numLetters)
 
-    val numLettersOnlyS = "Mississippi".count({ letter -> letter == 's'})
+    val numLettersOnlyS = "Mississippi".count({ letter -> letter == 's' })
     println(numLettersOnlyS)
 
     println("Mississippi".count { it == 's' })
 }
 
-private fun printPlayerStatus(
-    auraColor: String,
-    isBlessed: Boolean,
-    name: String,
-    healthStatus: String
-) {
-    println("(Aura: $auraColor) (Blessed: ${if (isBlessed) "YES" else "NO"})")
-    println("$name $healthStatus")
+private fun printPlayerStatus(player: Player) {
+    println(
+        "(Aura: ${player.auraColor()}) " +
+                "(Blessed: ${if (player.isBlessed) "YES" else "NO"})"
+    )
+    println("${player.name} ${player.formatHealthStatus()}")
 }
 
-private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean) =
-    if (isBlessed && healthPoints > 50 || isImmortal) {
-        "Green"
-    } else {
-        "None"
-    }
-
-private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) =
-    when (healthPoints) {
-        100 -> "최상의 상태임!"
-        in 90..99 -> "약간의 찰과상만 있음"
-        in 75..89 -> if (isBlessed) {
-            "경미한 상처가 있지만 빨리 치유됨."
-        } else {
-            " 경미한 상처만 있음"
-        }
-        in 15..74 -> "많이 다친 것 같음"
-        else -> "최악의 상태임!"
-    }
