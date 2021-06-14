032fa549fba29fa4401cfb8c1a433d351c175517
dec7 <denux7@gmail.com>
2021-06-15 00:26:10 +0900

    [KotlinProgramming][Ch15] sealed 클래스 적용

--- books/kotlin-programming/NyetHack/src/test/Student.kt
+++ books/kotlin-programming/NyetHack/src/test/Student.kt
@@ -1,21 +1,22 @@
 package test
 
-enum class StudentStatus {
-    NOT_ENROLLED,
-    ACTIVE,
-    GRADUATED
+sealed class StudentStatus {
+    object NotEnrolled: StudentStatus()
+    class Active(val courseId: String) : StudentStatus()
+    object Graduated: StudentStatus()
 }
 
 class Student(var status: StudentStatus)
 
 fun main(args: Array<String>) {
-    val student = Student(StudentStatus.NOT_ENROLLED)
+    val student = Student(StudentStatus.NotEnrolled)
+    println(studentMessage(student.status))
 }
 
 fun studentMessage(status: StudentStatus): String {
     return when(status) {
-        StudentStatus.NOT_ENROLLED -> "과정에 등록하세요!"
-        StudentStatus.ACTIVE -> "환영합니다"
-        StudentStatus.GRADUATED -> "졸업을 축하합니다."
+        is StudentStatus.NotEnrolled -> "과정에 등록하세요!"
+        is StudentStatus.Active -> "${status.courseId} 과정에 등록되었습니다."
+        is StudentStatus.Graduated -> "졸업을 축하합니다."
     }
 }
