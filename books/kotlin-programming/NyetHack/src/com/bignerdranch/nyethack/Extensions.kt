27475afc1f940325b2e2576cff1273bc3b25e8ea
dec7 <denux7@gmail.com>
2021-06-15 18:51:11 +0900

    [KotlinProgramming][Ch18] 제네릭 확장함수 정의

--- books/kotlin-programming/NyetHack/src/com/bignerdranch/nyethack/Extensions.kt
+++ books/kotlin-programming/NyetHack/src/com/bignerdranch/nyethack/Extensions.kt
@@ -2,9 +2,12 @@ package com.bignerdranch.nyethack
 
 fun String.addEnthusiasm(amount: Int = 1) = this + "!".repeat(amount)
 
-fun Any.easyPrint() = println(this)
+fun <T> T.easyPrint() : T {
+    println(this)
+    return this
+}
 
 fun main(args: Array<String>) {
-    "마드리갈이 그 건물에서 나왔습니다".addEnthusiasm().easyPrint()
+    "마드리갈이 그 건물에서 나왔습니다".easyPrint().addEnthusiasm().easyPrint()
     42.easyPrint()
 }
