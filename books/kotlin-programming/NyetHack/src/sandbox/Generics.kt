bf5ea59fcdbb853c93e813e75bcf018b26a48349
dec7 <denux7@gmail.com>
2021-06-15 17:13:55 +0900

    [KotlinProgramming][Ch17] 제네릭 함수 정의

--- books/kotlin-programming/NyetHack/src/sandbox/Generics.kt
+++ books/kotlin-programming/NyetHack/src/sandbox/Generics.kt
@@ -1,7 +1,12 @@
 package sandbox
 
 class LootBox<T>(item: T) {
+    var open = false
     private var loot: T = item
+
+    fun fetch(): T? {
+        return loot.takeIf { open }
+    }
 }
 
 class Fedora(val name: String, val value: Int)
@@ -11,4 +16,9 @@ class Coin(val value: Int)
 fun main(args: Array<String>) {
     val lootBoxOne: LootBox<Fedora> = LootBox(Fedora("평범한 중절모", 15))
     val lootBoxTwo: LootBox<Coin> = LootBox(Coin(15))
+
+    lootBoxOne.open = true
+    lootBoxOne.fetch()?.run {
+        println("$name 를 LootBox에서 꺼냈습니다.")
+    }
 }
