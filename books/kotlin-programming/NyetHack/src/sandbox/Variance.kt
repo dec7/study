2958cd0befefa85c088d3bea7b0011a18603765f
dec7 <denux7@gmail.com>
2021-06-15 18:21:33 +0900

    [KotlinProgramming][Ch17] in 키워드 지정

--- books/kotlin-programming/NyetHack/src/sandbox/Variance.kt
+++ books/kotlin-programming/NyetHack/src/sandbox/Variance.kt
@@ -1,11 +1,10 @@
 package sandbox
 
-class Barrel<out T>(val item: T)
+class Barrel<in T>(item: T)
 
 fun main(args: Array<String>) {
     var fedoraBarrel: Barrel<Fedora> = Barrel(Fedora("평범한 중절모", 15))
     var lootBarrel: Barrel<Loot> = Barrel(Coin(15))
 
-    lootBarrel = fedoraBarrel
-    val myFedora: Fedora = lootBarrel.item
+    fedoraBarrel = lootBarrel
 }
